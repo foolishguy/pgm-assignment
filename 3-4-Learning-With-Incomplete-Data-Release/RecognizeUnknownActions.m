e9fbe058642646b2a3bd459d8717f66012814b08
baozhehui <baozhehui@dianwoba.com>
2017-06-27 19:31:16 +0800

    RecognizeUnknownActions not finished

---
+++ 3-4-Learning-With-Incomplete-Data-Release/RecognizeUnknownActions.m
@@ -0,0 +1,5 @@
+% You should put all your code for recognizing unknown actions in this file.
+% Describe the method you used in YourMethod.txt.
+function [accuracy, predicted_labels] = RecognizeUnknownActions(datasetTrain, datasetTest, G, maxIter)
+
+end
